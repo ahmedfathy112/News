6cefd723900f17a69001fde2aa487160b618534d
Ahmed Fathi <98648216+ahmedfathy112@users.noreply.github.com>
2024-10-09 17:24:20 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,30 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const JwtToken = request.cookies.get("JwtToken");
   const token = JwtToken?.value as string;
+
+  // التحقق من وجود التوكن
   if (!token) {
-    return NextResponse.json({ message: "Accsess denaid!" }, { status: 401 });
+    return NextResponse.json({ message: "Access denied!" }, { status: 401 });
   }
+
+  const response = NextResponse.next();
+
+  // إعدادات CORS
+  response.headers.set("Access-Control-Allow-Origin", "*"); // السماح لجميع النطاقات
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  // معالجة طلبات OPTIONS
+  if (request.method === "OPTIONS") {
+    return new NextResponse(null, { status: 204 });
+  }
+
+  return response;
 }
 
 export const config = {
-  matcher: "/api/users/profile/:path*",
+  matcher: "/api/users/profile/:path*", // يمكنك تعديل هذه القيمة لتناسب المسارات التي ترغب في تطبيق الميدل وير عليها
 };
